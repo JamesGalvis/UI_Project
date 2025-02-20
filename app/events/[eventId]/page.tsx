81aa6e8dbbb8faa92d43fc2a7e94c91ff2485614
James Galvis <jamesrgal@gmail.com>
2025-02-20 15:33:53 -0500

    Events comments

--- app/events/[eventId]/page.tsx
+++ app/events/[eventId]/page.tsx
@@ -1,20 +1,25 @@
-import { Podcast } from "@/components/dinamic/events/podcast"
-import { Clapperboard } from "lucide-react"
-import Image from "next/image"
-import { EventImages } from "./components/event-images"
-import { getEvent } from "@/actions/get-events"
-import { formatDate } from "@/utils/format-date"
+import { Podcast } from "@/components/dinamic/events/podcast";
+import { Clapperboard } from "lucide-react";
+import Image from "next/image";
+import { EventImages } from "./components/event-images";
+import { getEvent } from "@/actions/get-events";
+import { formatDate } from "@/utils/format-date";
+import { EventCommentForm } from "@/components/dinamic/events/event-comment-form";
 
-export default async function EventPage({ params }: { params: { eventId: string } }) {
-  const eventId = params.eventId
-  const event = await getEvent(eventId)
+export default async function EventPage({
+  params,
+}: {
+  params: { eventId: string };
+}) {
+  const eventId = params.eventId;
+  const event = await getEvent(eventId);
 
-  const startDate = formatDate(new Date(event.startDate))
-  const endDate = formatDate(new Date(event.endDate))
+  const startDate = formatDate(new Date(event.startDate));
+  const endDate = formatDate(new Date(event.endDate));
 
   return (
-    <div className="min-h-full bg-zinc-100/30">
-      <header className="relative h-96">
+    <div className="min-h-full bg-zinc-100/30 overflow-x-hidden">
+      <header className="relative h-96 lg:mt-[86px] sm:mt-[148.84px] mt-[62px]">
         <Image
           src={event?.billboard!}
           alt="Imagen de fondo del evento"
@@ -39,10 +44,14 @@ export default async function EventPage({ params }: { params: { eventId: string 
               <p className="text-gray-600 mb-4">{event?.description}</p>
               <div className="mb-4 space-y-1">
                 <div className="flex gap-1">
-                  <h3 className="font-semibold">Inicio: <span className="font-normal">{startDate}</span></h3>
+                  <h3 className="font-semibold">
+                    Inicio: <span className="font-normal">{startDate}</span>
+                  </h3>
                 </div>
                 <div className="flex gap-1">
-                  <h3 className="font-semibold">Fin: <span className="font-normal">{endDate}</span></h3>
+                  <h3 className="font-semibold">
+                    Fin: <span className="font-normal">{endDate}</span>
+                  </h3>
                 </div>
                 <div className="md:col-span-2">
                   <h3 className="font-semibold">Ubicación:</h3>
@@ -79,40 +88,11 @@ export default async function EventPage({ params }: { params: { eventId: string 
             <section className="">
               <h2 className="text-2xl font-bold mb-4">Imágenes del Evento</h2>
               <EventImages images={event.images} />
-              {/* <div className="grid xs:grid-cols-2 gap-4">
-                <Image
-                  src={event?.billboard!}
-                  alt="Imagen del evento 1"
-                  width={400}
-                  height={300}
-                  className="rounded-lg aspect-square object-cover"
-                />
-                <Image
-                  src={event?.billboard!}
-                  alt="Imagen del evento 2"
-                  width={400}
-                  height={300}
-                  className="rounded-lg aspect-square object-cover"
-                />
-                <Image
-                  src={event?.billboard!}
-                  alt="Imagen del evento 3"
-                  width={400}
-                  height={300}
-                  className="rounded-lg aspect-square object-cover"
-                />
-                <Image
-                  src={event?.billboard!}
-                  alt="Imagen del evento 4"
-                  width={400}
-                  height={300}
-                  className="rounded-lg aspect-square object-cover"
-                />
-              </div> */}
             </section>
+            <EventCommentForm eventId={eventId} />
           </div>
         </div>
       </main>
     </div>
-  )
+  );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 import { ReactLenis } from "lenis/react"
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({
           <Navbar />
           {children}
           <Footer />
+          <Toaster richColors />
         </body>
       </ReactLenis>
     </html>
